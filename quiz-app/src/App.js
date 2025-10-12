3bd15bc5757cfff431211f9bbe6d43704aea4020
Richmondfreeheart <142225053+Richmondfreeheart@users.noreply.github.com>
2025-10-12 16:40:56 +0000

    Update App.js

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import QuizStart from "./components/QuizStart";
+import QuestionCard from "./components/QuestionCard";
+import ScoreSummary from "./components/ScoreSummary";
+
 function App() {
+  const [quizState, setQuizState] = useState("start"); // start | playing | end
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const startQuiz = (fetchedQuestions) => {
+    setQuestions(fetchedQuestions);
+    setQuizState("playing");
+    setCurrentQuestion(0);
+    setScore(0);
+  };
+
+  const handleAnswer = (isCorrect) => {
+    if (isCorrect) setScore((prev) => prev + 1);
+    if (currentQuestion + 1 < questions.length) {
+      setCurrentQuestion((prev) => prev + 1);
+    } else {
+      setQuizState("end");
+    }
+  };
+
+  const restartQuiz = () => {
+    setQuizState("start");
+    setQuestions([]);
+    setCurrentQuestion(0);
+    setScore(0);
+  };
+
   return (
     <div>
-      <h1>Hello, Quiz App!</h1>
+      {quizState === "start" && <QuizStart startQuiz={startQuiz} />}
+      {quizState === "playing" && (
+        <QuestionCard
+          question={questions[currentQuestion]}
+          handleAnswer={handleAnswer}
+        />
+      )}
+      {quizState === "end" && (
+        <ScoreSummary score={score} total={questions.length} restartQuiz={restartQuiz} />
+      )}
     </div>
   );
 }
